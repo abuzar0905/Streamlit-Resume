ef60fee980db39e50f918af95da39d36a50541cf
abuzar0905 <abuzar0905@gmail.com>
2025-10-04 22:37:05 +0800

    Update Resume.py

--- Resume.py
+++ Resume.py
@@ -4,7 +4,7 @@ import streamlit as st
 col1, col2 = st.columns([1, 3])
 
 with col1:
-st.image("Passport Size.jpg", caption="Abu Zar", width=180)
+  st.image("Passport Size.jpg", caption="Abu Zar", width=180)
 
 with col2:
 st.title("Abu Zar's Resume")
