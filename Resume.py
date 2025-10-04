270b6e481d7d84acd0f354722a9ed71b208c411a
abuzar0905 <abuzar0905@gmail.com>
2025-10-04 22:35:27 +0800

    Update Resume.py

--- Resume.py
+++ Resume.py
@@ -1,26 +1,33 @@
 import streamlit as st
 
-st.title("Abu Zar's Resume")
+# Profile
+col1, col2 = st.columns([1, 3])
 
+with col1:
 st.image("Passport Size.jpg", caption="Abu Zar", width=180)
 
-st.header("Contact Information")
-st.write("Email: abuzar0905@gmail.com")
-st.write("Phone: (+60) 11-26612895")
+with col2:
+st.title("Abu Zar's Resume")
+st.write("📧 **Email:** abuzar0905@gmail.com")
+st.write("📱 **Phone:** (+60) 11-26612895")
 
-st.header("Education")
-st.write("Degree in Information Technology, Universiti Malaysia Kelantan, 2023 - 2026")
+#Education
+st.header("🎓 Education")
+st.write("Degree in Information Technology, Universiti Malaysia Kelantan (2023 - 2026)")
 
-st.header("Work Experience")
-st.write("Technician, Suruhanjaya Pilihan Raya, 2019")
-st.write("- Managing and maintaining every automated systems in the buildings(e.g Elevator, lift, AC, PA systems, lighting).")
+#Work
+st.header("💼 Work Experience")
+st.write("Technician, Suruhanjaya Pilihan Raya (2019)")
+st.write("- Managed and maintained every automated systems in the buildings(e.g Elevator, lift, AC, PA systems, lighting).")
 
-st.header("Skills")
+#Skills
+st.header("🛠️ Skills")
 st.write("- Internet Networking")
 st.write("- Electrical wiring")
 st.write("- IOT systems")
 st.write("- Professional Audio")
 st.write("- Server OS (Windows & Linux)")
 
-st.header("Projects")
+#Project
+st.header("📂 Project")
 st.write("NinJaVan Smart Tracking Systems")
